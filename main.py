6a932a34e903394501927449fed86c2577ad730e
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-02-12 18:50:02 +0500

    90% done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from card.card_api import card_router
+
+from currency.currency_api import currency_router
+
 from transfers.transfers_api import trans_router
 from users.user_api import user_router
 
@@ -12,3 +15,4 @@ app = FastAPI(docs_url='/')
 app.include_router(card_router)
 app.include_router(user_router)
 app.include_router(trans_router)
+app.include_router(currency_router)
